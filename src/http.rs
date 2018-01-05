a4eacf493a39b87857ce93a96dc288fc6d2aa21a
minauteur <Thompennington@gmail.com>
2018-01-05 14:55:47 -0600

    adds basic lines collection from fn pretty_print() {...}

--- src/http.rs
+++ src/http.rs
@@ -10,6 +10,7 @@ use std::io;
 
 use serde_json;
 use serde::{Serialize, Deserialize};
+use serde;
 
 use text_io;
 use std::collections::HashMap;
@@ -23,6 +24,13 @@ pub struct AuthorsList {
 pub struct AuthorWorks {
     author: String,
     works: Vec<SinglePoem>,
+}
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Poem {
+    author: String,
+    linecount: i32,
+    lines: Vec<String>,
+    title: String,    
 }
 
 pub struct SinglePoem {
@@ -99,6 +107,7 @@ impl BasicSearch {
         let mut req: Request = RequestBuilder::new().with_params(author, title);
         let resp = get_response(req);
         let serialized = serialize(resp);
+        // let poem = get_lines(serialized);
         let string = pretty_print(serialized);
         return BasicSearch {
             results: match string {
@@ -121,12 +130,15 @@ impl Request {
             None => String::from(""),
         };
         if author.is_some() && title.is_none() {
+            println!("No title given... \nreturning list of titles for names matching substrings to provided input...");
             let single_author = format!("author/{}/title", &a.trim_right());
             &self.url.push_str(&single_author);
         } else if author.is_none() && title.is_none() {
+            println!("No author or title given... \nreturning list of authors...");
             let authors = format!("author");
             &self.url.push_str(&authors);
         } else if author.is_some() && title.is_some() {
+            println!("searching for substring matches by author name and work title given...");
             let author_title = format!("author,title/{};{}", &a.trim_right(), &t.trim_right());
             &self.url.push_str(&author_title);
         } else if author.is_none() && title.is_some() {
@@ -149,6 +161,10 @@ pub fn serialize(
 ) -> reqwest::Result<(serde_json::Value)> {
     if resp.is_ok() {
         let data: serde_json::Value = resp.unwrap().json()?;
+        // let d_str: String = serde_json::to_string(&data.clone()).unwrap();
+        // let poem: Poem = serde_json::from_str(&d_str).unwrap();
+        // println!("no problem, here are the lines! \n{:?}", &poem);
+        println!("here is our serde value: {:?}", &data[0]);
         return Ok((data));
     } else {
         return Err(resp.unwrap_err());
@@ -157,7 +173,9 @@ pub fn serialize(
 
 pub fn pretty_print(res: reqwest::Result<(serde_json::Value)>) -> serde_json::Result<(String)> {
     if res.is_ok() {
-        let j_string = serde_json::to_string_pretty(&res.unwrap())?;
+        let j_string = serde_json::to_string_pretty(&res.unwrap()[0])?;
+        let p: Poem = serde_json::from_str(&j_string)?;
+        println!("got lines! {:?}", p.lines);
         println!("json from pretty_print(): {}", &j_string);
         return Ok((j_string));
     } else {
